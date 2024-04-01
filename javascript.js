5e5df69bba12454dc40b199dcf1df25711286faa
Ryan <ryan.irani@gmail.com>
2024-04-01 17:48:11 +0100

    added UI

--- javascript.js
+++ javascript.js
@@ -1,16 +1,30 @@
+const gameContainer = document.querySelector(".game");
 
-function gameboard(){
+(function drawBoard(){
     const rows = 3;
     const columns = 3;
     const gameboard = [];
-
+    let table = document.createElement("div");
+    table.classList.add("table");
+    
     for (let i = 0; i < rows; i++){
         gameboard[i] = [];
         for (let j=0; j < columns; j++){
             gameboard[i].push(createCell());
+            let cell = document.createElement('div');
+            cell.classList.add("cell");
+            table.appendChild(cell);
+
+            function addClickListener(i){
+                cell.addEventListener("click", () => {
+                    cell.innerText = i;
+                });
+            }
+            addClickListener(i);
         }
+        gameContainer.appendChild(table);
     }
-}
+})();
 
 function createCell(){    
     let value = 0;
@@ -19,27 +33,25 @@ function createCell(){
     }
     return {
         addUserInput
-    };
-
-} 
-
-function createPlayer(name, symbol){
-    return {
-        name,
-        symbol,
-        introduction(){
-            return `Welcome ${name}` 
-        }
     }
-}
 
-const playerOne = createPlayer("Player One", "O");
-const playerTwo = createPlayer("Player Two", "X");
+};
 
+function gameController(){
 
-function gameboardObjects(){
+};
 
-}
+// function createPlayer(name, symbol){
+//     return {
+//         name,
+//         symbol,
+//         introduction(){
+//             return `Welcome ${name}` 
+//         }
+//     }
+// }
 
-gameboard();
+// const playerOne = createPlayer("Player One", "O");
+// const playerTwo = createPlayer("Player Two", "X");
+
 
