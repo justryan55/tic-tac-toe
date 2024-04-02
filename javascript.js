0866473ccbea8ede16b1803f8393ba70c88346dd
Ryan <ryan.irani@gmail.com>
2024-04-02 23:27:32 +0100

    added winning combinations

--- javascript.js
+++ javascript.js
@@ -1,78 +1,87 @@
-const gameContainer = document.querySelector(".game");
 
-(function drawBoard(){
-    const rows = 3;
-    const columns = 3;
-    const gameboard = [];
-    let table = document.createElement("div");
-    table.classList.add("table");
-    
-    for (let i = 0; i < rows; i++){
-        gameboard[i] = [];
-        for (let j=0; j < columns; j++){
-            gameboard[i].push(createCell());
-            let cell = document.createElement('div');
-            cell.classList.add("cell");
-            table.appendChild(cell);
+function Cell(tableElement, cellClicked, cellUpdated) {
+    this.value = null;
+    this.handleClick = function() {
+        if(this.value) {
+            return;
         }
-        gameContainer.appendChild(table);
-    }
-})();
-
-function createCell(){    
-    let value = 0;
-    const addUserInput = () => {
-        // value = 
-    }
-    return {
-        addUserInput
+        const turn = cellClicked();
+        cellElement.innerText = (turn ? "x" : "o");
+        this.value = (turn ? "x" : "o");
+        cellUpdated();
     }
 
-};
+    let cellElement = document.createElement('div');
+    cellElement.classList.add("cell");
 
-(function gameController(){
-    const gameboardSquares = gameContainer.querySelectorAll(".table div");
-    const gameboardSquaresArray = Array.from(gameboardSquares);
-    const gameText = document.querySelector(".text");
-    const playerTurn = [];
-    const storeSquareIndex = [];
-    
-    function createPlayer(name, symbol){
-            return {
-                name,
-                symbol,
-                displayTurn(){
-                    return gameText.innerText = `It is ${name}'s turn`; 
-                }
-            }
-        }
-        
-    const playerOne = createPlayer("Player One", "O");
-    const playerTwo = createPlayer("Player Two", "X");
-
-    function changeTurn(){
-        
-    }
-
-    function userInput(){
-        console.log(storeSquareIndex);
-        changeTurn();
-        checkWinner();
-    }
-
-    function checkWinner(){
-      
-    }
-
-    gameboardSquaresArray.forEach((square) => {
-        square.addEventListener("click", () => {
-            let squareIndex = gameboardSquaresArray.indexOf(square);
-            storeSquareIndex.push(squareIndex); 
-            userInput();
-        });
+    cellElement.addEventListener('click', () => {
+        this.handleClick();
     })
 
-})();
+    tableElement.appendChild(cellElement);
+}
 
+function TicTacToeGame() {
+    this.rows = 3;
+    this.columns = 3;
+    this.gameboard = [];
+    this.gameContainer = document.querySelector(".game");
+    this.gameText = document.querySelector(".text");
+    this.playAgainText = document.querySelector(".play-again");
+    this.start = function() {
+        this.drawBoard();
+    }
+    this.drawBoard = () => {
+        let table = document.createElement("div");
+        table.classList.add("table");
+        
+        for (let i = 0; i < this.rows; i++){
+            this.gameboard[i] = [];
+            for (let j=0; j < this.columns; j++) {
+                const cell = new Cell(table, () => {
+                    const t = this.isPlayerOnesTurn;
+                    this.isPlayerOnesTurn = !this.isPlayerOnesTurn;
+                    return t;
+                }, () => {
+                    this.checkIfThereIsAWinner();
+                });
+                this.gameboard[i].push(cell);
+            }
+            this.gameContainer.appendChild(table);
+        }
+    }
+    this.checkIfThereIsAWinner = () => {
+        for (let index = 0; index < 3; index++) {
+            if(this.gameboard[index][0].value && (this.gameboard[index][0].value === this.gameboard[index][1].value) && (this.gameboard[index][1].value === this.gameboard[index][2].value)) {
+                this.gameText.innerText = `${this.gameboard[index][0].value} wins!`;
+                this.playRound();
+            } else if(this.gameboard[1][index].value && (this.gameboard[0][index].value === this.gameboard[1][index].value) && (this.gameboard[1][index].value === this.gameboard[2][index].value)) {
+                this.gameText.innerText = `${this.gameboard[0][index].value} wins!`;
+                this.playRound();
+            } else if (this.gameboard[index][index].value && (this.gameboard[0][0].value === this.gameboard[1][1].value) && (this.gameboard[1][1].value === this.gameboard[2][2].value)) {
+                this.gameText.innerText = `${this.gameboard[index][index].value} wins!`;
+                this.playRound();
+            } else if ((this.gameboard[2][0].value || this.gameboard[0][2].value) && (this.gameboard[0][2].value === this.gameboard[1][1].value) && (this.gameboard[1][1].value === this.gameboard[2][0].value)) {
+                this.gameText.innerText = `${this.gameboard[index][0].value} wins!`;
+                this.playRound(); 
+            // } else if (!this.gameboard.value){ Need to include login for if board fills up with no winner
+            //     alert("Hello")
+            }
+        }
+    }
 
+    this.playRound = () => {
+        if (this.checkIfThereIsAWinner){
+            this.playAgainText.innerText = "Play again?";
+            this.playAgainText.addEventListener("click", this.resetGame)
+        }
+    }
 
+    this.resetGame = () => {
+        this.gameboard = [];
+        this.gameContainer = [];
+    }
+}
+
+const ticTacToe = new TicTacToeGame();
+ticTacToe.start();
